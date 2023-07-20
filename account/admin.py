99f8506f0c809514d93af2f1ccb833b1b8520067
md tarik bosunia <bosuniamdtarik005@gmail.com>
2023-07-20 19:53:05 +0600

    edit account/admin.py for displaying user better in admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from account.models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        ('Credentials', {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone', 'profile_image')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_admin', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('created_at', 'updated_at')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'username', 'first_name', 'last_name', 'phone', 'is_staff')
+    list_display_links = ('email', 'username', 'first_name', 'last_name')
+    search_fields = ('email', 'username', 'first_name', 'last_name', 'phone')
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ('email', '-created_at',)
+
+
+admin.site.register(User, UserAdmin)
